cf3640cbb7ee2c9a74618985f1e5859f83c76c02
gfreezy <gfreezy@gmail.com>
2019-11-17 20:10:36 +0800

    add gateway mode

--- sysconfig/src/net/darwin.rs
+++ sysconfig/src/net/darwin.rs
@@ -1,28 +1,12 @@
-use std::process::Command;
+use crate::net::run_cmd;
 use tracing::info;
 
 pub struct DNSSetup;
 
 impl DNSSetup {
+    #[allow(clippy::new_without_default)]
     pub fn new() -> Self {
-        DNSSetup::default()
-    }
-}
-
-impl Default for DNSSetup {
-    fn default() -> Self {
-        info!("setup dns");
-        let output = Command::new("networksetup")
-            .args(&["-setdnsservers", "Wi-Fi", "127.0.0.1"])
-            .output()
-            .expect("setup local dns");
-        if !output.status.success() {
-            panic!(
-                "stdout: {}\nstderr: {}",
-                std::str::from_utf8(&output.stdout).expect("utf8"),
-                std::str::from_utf8(&output.stderr).expect("utf8")
-            );
-        }
+        let _ = run_cmd("networksetup", &["-setdnsservers", "Wi-Fi", "127.0.0.1"]);
         DNSSetup
     }
 }
@@ -30,43 +14,11 @@ impl Default for DNSSetup {
 impl Drop for DNSSetup {
     fn drop(&mut self) {
         info!("clear dns");
-        let output = Command::new("networksetup")
-            .args(&["-setdnsservers", "Wi-Fi", "empty"])
-            .output()
-            .expect("clear local dns");
-        if !output.status.success() {
-            panic!(
-                "stdout: {}\nstderr: {}",
-                std::str::from_utf8(&output.stdout).expect("utf8"),
-                std::str::from_utf8(&output.stderr).expect("utf8")
-            );
-        }
+        let _ = run_cmd("networksetup", &["-setdnsservers", "Wi-Fi", "empty"]);
     }
 }
 
 pub fn setup_ip(tun_name: &str, ip: &str, cidr: &str) {
-    let output = Command::new("ifconfig")
-        .args(&[tun_name, ip, ip])
-        .output()
-        .expect("run ifconfig");
-    if !output.status.success() {
-        panic!(
-            "stdout: {}\nstderr: {}",
-            std::str::from_utf8(&output.stdout).expect("utf8"),
-            std::str::from_utf8(&output.stderr).expect("utf8")
-        );
-    }
-    let output = Command::new("route")
-        .arg("add")
-        .arg(cidr)
-        .arg(ip)
-        .output()
-        .expect("add route");
-    if !output.status.success() {
-        panic!(
-            "stdout: {}\nstderr: {}",
-            std::str::from_utf8(&output.stdout).expect("utf8"),
-            std::str::from_utf8(&output.stderr).expect("utf8")
-        );
-    }
+    let _ = run_cmd("ifconfig", &[tun_name, ip, ip]);
+    let _ = run_cmd("route", &["add", cidr, ip]);
 }
