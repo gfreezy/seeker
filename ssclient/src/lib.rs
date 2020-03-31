2e648cb862c3d56dbef1bfee5c5194c8704114e0
Alex.F <gfreezy@gmail.com>
2020-03-31 22:24:58 +0800

    fix deadlock

--- ssclient/src/lib.rs
+++ ssclient/src/lib.rs
@@ -5,12 +5,3 @@ const BUFFER_SIZE: usize = 8 * 1024; // 8K buffer
 
 pub use tcp_io::SSTcpStream;
 pub use udp_io::SSUdpSocket;
-
-#[cfg(test)]
-fn setup_tracing_subscriber() {
-    use tracing_subscriber::fmt::Subscriber;
-    use tracing_subscriber::EnvFilter;
-
-    let builder = Subscriber::builder().with_env_filter(EnvFilter::new("ssclient=trace"));
-    builder.try_init().unwrap();
-}
