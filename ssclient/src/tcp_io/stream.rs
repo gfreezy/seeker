c2c8499e87aa624cbc9999098e67a4172714ecbb
Alex.F <gfreezy@gmail.com>
2020-05-26 22:05:48 +0800

    fix tests

--- ssclient/src/tcp_io/stream.rs
+++ ssclient/src/tcp_io/stream.rs
@@ -206,9 +206,9 @@ mod tests {
     use super::{DecryptedReader, EncryptedWriter};
     use async_std::io::Cursor;
     use async_std::prelude::*;
+    use async_std::task::block_on;
     use bytes::Bytes;
     use crypto::{CipherType, CryptoMode};
-    use futures::executor::block_on;
 
     #[test]
     fn test_write() {
