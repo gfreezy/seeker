b0e70b96dfb20e8403b0968c2c5d857d98d35081
Alex.F <gfreezy@gmail.com>
2020-06-03 00:09:28 +0800

    use trust-dns

--- seeker/src/dns_client.rs
+++ seeker/src/dns_client.rs
@@ -1,67 +1,46 @@
+use async_std_resolver::config::{NameServerConfigGroup, ResolverConfig, ResolverOpts};
+use async_std_resolver::{resolver, AsyncStdResolver};
 use config::Address;
-use hermesdns::{DnsClient as _, DnsNetworkClient, DnsRecord, QueryType, TransientTtl};
-use lru_time_cache::LruCache;
-use parking_lot::Mutex;
 use std::io::{Error, ErrorKind, Result};
 use std::net::IpAddr;
 use std::net::SocketAddr;
-use std::sync::Arc;
 use std::time::Duration;
 
 #[derive(Clone)]
 pub struct DnsClient {
-    dns_server: String,
-    dns_port: u16,
-    client: DnsNetworkClient,
-    cache: Arc<Mutex<LruCache<String, IpAddr>>>,
+    resolver: AsyncStdResolver,
 }
 
 impl DnsClient {
     pub async fn new(dns_server: SocketAddr, timeout: Duration) -> Self {
-        let client = DnsNetworkClient::new(0, timeout).await;
-        DnsClient {
-            dns_server: dns_server.ip().to_string(),
-            dns_port: dns_server.port(),
-            client,
-            cache: Arc::new(Mutex::new(LruCache::with_capacity_and_auto_expiration(100))),
-        }
+        // Construct a new Resolver with default configuration options
+        let resolver = resolver(
+            ResolverConfig::from_parts(
+                None,
+                Vec::new(),
+                NameServerConfigGroup::from_ips_clear(&[dns_server.ip()], dns_server.port()),
+            ),
+            ResolverOpts {
+                timeout,
+                ..Default::default()
+            },
+        )
+        .await
+        .expect("failed to create resolver");
+
+        DnsClient { resolver }
     }
 
     pub async fn lookup(&self, domain: &str) -> Result<IpAddr> {
-        if let Some(addr) = self.cache.lock().get(domain) {
-            return Ok(*addr);
-        }
-
-        let packet = self
-            .client
-            .send_query(
-                domain,
-                QueryType::A,
-                (self.dns_server.as_ref(), self.dns_port),
-                true,
-            )
-            .await?;
-
-        for answer in &packet.answers {
-            if let DnsRecord::A {
-                domain,
-                addr: ip,
-                ttl: TransientTtl(ttl),
-            } = answer
-            {
-                let _ = self.cache.lock().insert_with_ttl(
-                    domain.to_string(),
-                    IpAddr::V4(*ip),
-                    Duration::from_secs(*ttl as u64),
-                );
-                return Ok(IpAddr::V4(*ip));
-            };
-        }
-
-        Err(Error::new(
-            ErrorKind::NotFound,
-            format!("{} not resolved", domain),
-        ))
+        let response = self
+            .resolver
+            .lookup_ip(domain)
+            .await
+            .map_err(|_| Error::new(ErrorKind::NotFound, format!("{} not resolved", domain)))?;
+        response
+            .iter()
+            .next()
+            .ok_or_else(|| Error::new(ErrorKind::NotFound, format!("{} not resolved", domain)))
     }
 
     pub async fn lookup_address(&self, addr: &Address) -> Result<SocketAddr> {
