842186d5326e2d7d86c4432dce46dad6e6d4af14
Alex.F <gfreezy@gmail.com>
2020-06-09 23:31:33 +0800

    fix

--- sysconfig/src/lib.rs
+++ sysconfig/src/lib.rs
@@ -1,9 +1,11 @@
 mod command;
 mod net;
+#[cfg(target_arch = "x86_64")]
 mod proc;
 mod ulimit;
 
 pub use net::{setup_ip, DNSSetup, IpForward};
+#[cfg(target_arch = "x86_64")]
 pub use proc::sys::{list_system_proc_socks, list_user_proc_socks};
 pub use proc::SocketInfo;
 pub use ulimit::{get_rlimit_no_file, set_rlimit_no_file};
