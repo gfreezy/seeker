9184c000f7c09d75268415e0017ea5e9649574b0
Alex.F <gfreezy@gmail.com>
2020-06-09 22:33:04 +0800

    update

--- sysconfig/src/ulimit.rs
+++ sysconfig/src/ulimit.rs
@@ -1,9 +1,15 @@
 use std::io;
 
 pub fn set_rlimit_no_file(no: u64) -> io::Result<()> {
+    #[cfg(not(target_arch = "arm"))]
     let rlim = libc::rlimit {
         rlim_cur: no,
         rlim_max: no,
+    };
+    #[cfg(target_arch = "arm")]
+    let rlim = libc::rlimit {
+        rlim_cur: no as u32,
+        rlim_max: no as u32,
     };
     let ret = unsafe { libc::setrlimit(libc::RLIMIT_NOFILE, &rlim) };
     if ret == -1 {
