aa2e99267ddfbad8fb6489c1616576f41098c6c8
Alex.F <gfreezy@gmail.com>
2022-10-25 15:44:57 +0800

    support remote ss url

--- config/src/server_config.rs
+++ config/src/server_config.rs
@@ -1,10 +1,14 @@
+use std::str::FromStr;
+use std::{error, fmt};
 use std::{fmt::Debug, net::SocketAddr};
 
 use crate::Address;
+use base64::{decode_config, URL_SAFE_NO_PAD};
 use bytes::Bytes;
 use crypto::CipherType;
 use serde::Deserialize;
 use tcp_connection::ObfsMode;
+use tracing::error;
 use url::Url;
 
 /// Server address
@@ -46,6 +50,14 @@ pub struct Obfs {
     pub mode: ObfsMode,
     pub host: String,
 }
+impl Obfs {
+    fn new(mode: ObfsMode, to_string: String) -> Self {
+        Obfs {
+            mode,
+            host: to_string,
+        }
+    }
+}
 
 mod cipher_type {
     use crypto::CipherType;
@@ -82,6 +94,26 @@ mod server_addr {
 }
 
 impl ServerConfig {
+    pub fn new(
+        name: String,
+        addr: Address,
+        protocol: ServerProtocol,
+        username: Option<String>,
+        password: Option<String>,
+        method: Option<CipherType>,
+        obfs: Option<Obfs>,
+    ) -> Self {
+        Self {
+            name,
+            addr,
+            protocol,
+            username,
+            password,
+            method,
+            obfs,
+        }
+    }
+
     /// Get server name
     pub fn name(&self) -> &str {
         &self.name
@@ -117,5 +149,229 @@ impl ServerConfig {
 
     pub fn obfs(&self) -> Option<&Obfs> {
         self.obfs.as_ref()
+    }
+
+    pub fn from_url(encoded: &str) -> Result<ServerConfig, UrlParseError> {
+        let parsed = Url::parse(encoded).map_err(UrlParseError::from)?;
+
+        if parsed.scheme() != "ss" {
+            return Err(UrlParseError::InvalidScheme);
+        }
+
+        let user_info = parsed.username();
+        if user_info.is_empty() {
+            // This maybe a QRCode URL, which is ss://BASE64-URL-ENCODE(pass:encrypt@hostname:port)
+
+            let encoded = match parsed.host_str() {
+                Some(e) => e,
+                None => return Err(UrlParseError::MissingHost),
+            };
+
+            let mut decoded_body = match decode_config(encoded, URL_SAFE_NO_PAD) {
+                Ok(b) => match String::from_utf8(b) {
+                    Ok(b) => b,
+                    Err(..) => return Err(UrlParseError::InvalidServerAddr),
+                },
+                Err(err) => {
+                    error!(
+                        "failed to parse legacy ss://ENCODED with Base64, err: {}",
+                        err
+                    );
+                    return Err(UrlParseError::InvalidServerAddr);
+                }
+            };
+
+            decoded_body.insert_str(0, "ss://");
+            // Parse it like ss://method:password@host:port
+            return ServerConfig::from_url(&decoded_body);
+        }
+
+        let (method, pwd) = match parsed.password() {
+            Some(password) => {
+                // Plain method:password without base64 encoded
+
+                let m = match percent_encoding::percent_decode_str(user_info).decode_utf8() {
+                    Ok(m) => m,
+                    Err(err) => {
+                        error!(
+                            "failed to parse percent-encoded method in userinfo, err: {}",
+                            err
+                        );
+                        return Err(UrlParseError::InvalidAuthInfo);
+                    }
+                };
+
+                let p = match percent_encoding::percent_decode_str(password).decode_utf8() {
+                    Ok(m) => m,
+                    Err(err) => {
+                        error!(
+                            "failed to parse percent-encoded password in userinfo, err: {}",
+                            err
+                        );
+                        return Err(UrlParseError::InvalidAuthInfo);
+                    }
+                };
+
+                (m, p)
+            }
+            None => {
+                let account = match decode_config(user_info, URL_SAFE_NO_PAD) {
+                    Ok(account) => match String::from_utf8(account) {
+                        Ok(ac) => ac,
+                        Err(..) => return Err(UrlParseError::InvalidAuthInfo),
+                    },
+                    Err(err) => {
+                        error!("failed to parse UserInfo with Base64, err: {}", err);
+                        return Err(UrlParseError::InvalidUserInfo);
+                    }
+                };
+
+                let mut sp2 = account.splitn(2, ':');
+                let (m, p) = match (sp2.next(), sp2.next()) {
+                    (Some(m), Some(p)) => (m, p),
+                    _ => return Err(UrlParseError::InvalidUserInfo),
+                };
+
+                (m.to_owned().into(), p.to_owned().into())
+            }
+        };
+
+        let host = match parsed.host_str() {
+            Some(host) => host,
+            None => return Err(UrlParseError::MissingHost),
+        };
+
+        let port = parsed.port().unwrap_or(8388);
+
+        let name_percent_encoding = parsed.fragment().unwrap_or(host);
+        let name = percent_encoding::percent_decode_str(name_percent_encoding)
+            .decode_utf8_lossy()
+            .to_string();
+        let method: CipherType =
+            CipherType::from_str(&method).map_err(|_| UrlParseError::InvalidProtocol)?;
+
+        let mut obfs_mode = None;
+        let mut obfs_host = None;
+        for (key, value) in parsed.query_pairs() {
+            if key != "plugin" {
+                continue;
+            }
+
+            let mut vsp = value.split(';');
+            // only obfs-local plugin is supported
+            if vsp.next() != Some("obfs-local") {
+                break;
+            }
+
+            for arg in vsp {
+                match arg.split_once('=') {
+                    Some(("obfs", "http")) => obfs_mode = Some(ObfsMode::Http),
+                    Some(("obfs-host", s)) => obfs_host = Some(s.to_string()),
+                    Some(other) => error!("Unsupported plugin argument: {:?}", other),
+                    None => {}
+                }
+            }
+        }
+
+        let obfs = match (obfs_mode, obfs_host) {
+            (Some(mode), Some(host)) => Some(Obfs::new(mode, host)),
+            (Some(mode), None) => Some(Obfs::new(mode, host.to_string())),
+            (None, Some(_)) => {
+                error!("obfs-host is set but obfs is not");
+                None
+            }
+            (None, None) => None,
+        };
+
+        let svrconfig = ServerConfig::new(
+            name,
+            Address::from_str(&format!("{}:{}", host, port)).unwrap(),
+            ServerProtocol::Shadowsocks,
+            None,
+            Some(pwd.to_string()),
+            Some(method),
+            obfs,
+        );
+
+        Ok(svrconfig)
+    }
+}
+
+/// Shadowsocks URL parsing Error
+#[derive(Debug, Clone)]
+pub enum UrlParseError {
+    ParseError(url::ParseError),
+    InvalidScheme,
+    InvalidUserInfo,
+    InvalidProtocol,
+    MissingHost,
+    InvalidAuthInfo,
+    InvalidServerAddr,
+    InvalidQueryString,
+}
+
+impl From<url::ParseError> for UrlParseError {
+    fn from(err: url::ParseError) -> UrlParseError {
+        UrlParseError::ParseError(err)
+    }
+}
+
+impl fmt::Display for UrlParseError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            UrlParseError::ParseError(ref err) => fmt::Display::fmt(err, f),
+            UrlParseError::InvalidScheme => write!(f, "URL must have \"ss://\" scheme"),
+            UrlParseError::InvalidUserInfo => write!(f, "invalid user info"),
+            UrlParseError::MissingHost => write!(f, "missing host"),
+            UrlParseError::InvalidAuthInfo => write!(f, "invalid authentication info"),
+            UrlParseError::InvalidServerAddr => write!(f, "invalid server address"),
+            UrlParseError::InvalidQueryString => write!(f, "invalid query string"),
+            UrlParseError::InvalidProtocol => write!(f, "invalid protocol"),
+        }
+    }
+}
+
+impl error::Error for UrlParseError {
+    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
+        match *self {
+            UrlParseError::ParseError(ref err) => Some(err as &dyn error::Error),
+            UrlParseError::InvalidScheme => None,
+            UrlParseError::InvalidUserInfo => None,
+            UrlParseError::InvalidProtocol => None,
+            UrlParseError::MissingHost => None,
+            UrlParseError::InvalidAuthInfo => None,
+            UrlParseError::InvalidServerAddr => None,
+            UrlParseError::InvalidQueryString => None,
+        }
+    }
+}
+
+impl FromStr for ServerConfig {
+    type Err = UrlParseError;
+
+    fn from_str(s: &str) -> Result<ServerConfig, Self::Err> {
+        ServerConfig::from_url(s)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_ss_url() -> Result<(), UrlParseError> {
+        let url = "ss://YWVzLTI1Ni1nY206MTE0NTE0@1eae257e44aa9d5b.jijifun.com:30002/?plugin=obfs-local%3Bobfs%3Dhttp%3Bobfs-host%3Dc61be5399e.microsoft.com#%E9%A6%99%E6%B8%AF-ByWave+01";
+        let server_config = ServerConfig::from_str(url)?;
+        assert_eq!(server_config.name, "香港-ByWave+01");
+        assert_eq!(server_config.protocol(), ServerProtocol::Shadowsocks);
+        assert_eq!(
+            server_config.obfs(),
+            Some(&Obfs {
+                mode: ObfsMode::Http,
+                host: "c61be5399e.microsoft.com".to_string()
+            })
+        );
+        assert_eq!(server_config.method(), Some(CipherType::Aes256Gcm));
+        Ok(())
     }
 }
