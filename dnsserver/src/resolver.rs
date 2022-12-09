9efaabd5d526746576be9f3e21418d232dcb6e3a
Alex.F <gfreezy@gmail.com>
2022-12-09 15:45:17 +0800

    support DNS TXT record

--- dnsserver/src/resolver.rs
+++ dnsserver/src/resolver.rs
@@ -1,4 +1,3 @@
-use async_std::net::IpAddr;
 use async_std_resolver::AsyncStdResolver;
 use async_trait::async_trait;
 use config::rule::{Action, ProxyRules};
@@ -11,8 +10,8 @@ use std::net::Ipv4Addr;
 use std::path::Path;
 use std::sync::atomic::{AtomicU32, Ordering};
 use std::sync::Arc;
-use tracing::{debug, error, trace};
-use trust_dns_proto::rr::RData;
+use tracing::{debug, error};
+use trust_dns_proto::rr::{RData, RecordType};
 
 const NEXT_IP: &str = "next_ip";
 
@@ -86,13 +85,98 @@ impl RuleBasedDnsResolver {
         addr.to_string()
     }
 
-    async fn resolve(&self, domain: &str) -> Result<DnsPacket> {
+    async fn resolve_real(&self, domain: &str, qtype: QueryType) -> Result<DnsPacket> {
         let mut packet = DnsPacket::new();
+        let lookup = self
+            .inner
+            .resolver
+            .lookup(domain, RecordType::from(qtype.to_num()))
+            .await
+            .map_err(|e| {
+                let msg = e.to_string();
+                error!("directly lookup host error: {}", &msg);
+                io::Error::new(io::ErrorKind::Other, msg)
+            })?;
+        for record in lookup.record_iter() {
+            let rdata = match record.data() {
+                None => {
+                    continue;
+                }
+                Some(RData::A(ip)) => DnsRecord::A {
+                    domain: domain.to_string(),
+                    addr: *ip,
+                    ttl: TransientTtl(record.ttl()),
+                },
+                Some(RData::AAAA(ip)) => DnsRecord::AAAA {
+                    domain: domain.to_string(),
+                    addr: *ip,
+                    ttl: TransientTtl(record.ttl()),
+                },
+                Some(RData::CNAME(cname)) => DnsRecord::CNAME {
+                    domain: domain.to_string(),
+                    host: cname.to_string(),
+                    ttl: TransientTtl(record.ttl()),
+                },
+                Some(RData::MX(mx)) => DnsRecord::MX {
+                    domain: domain.to_string(),
+                    host: mx.exchange().to_string(),
+                    priority: mx.preference(),
+                    ttl: TransientTtl(record.ttl()),
+                },
+                Some(RData::NS(ns)) => DnsRecord::NS {
+                    domain: domain.to_string(),
+                    host: ns.to_string(),
+                    ttl: TransientTtl(record.ttl()),
+                },
+                Some(RData::SOA(soa)) => DnsRecord::SOA {
+                    domain: domain.to_string(),
+                    m_name: soa.mname().to_string(),
+                    r_name: soa.rname().to_string(),
+                    serial: soa.serial(),
+                    refresh: soa.refresh() as u32,
+                    retry: soa.retry() as u32,
+                    expire: soa.expire() as u32,
+                    minimum: soa.minimum(),
+                    ttl: TransientTtl(record.ttl()),
+                },
+                Some(RData::TXT(txt)) => DnsRecord::TXT {
+                    domain: domain.to_string(),
+                    data: txt.to_string(),
+                    ttl: TransientTtl(record.ttl()),
+                },
+                Some(RData::SRV(srv)) => DnsRecord::SRV {
+                    domain: domain.to_string(),
+                    priority: srv.priority(),
+                    weight: srv.weight(),
+                    port: srv.port(),
+                    host: srv.target().to_string(),
+                    ttl: TransientTtl(record.ttl()),
+                },
+                other => {
+                    tracing::error!("unsupported record type: {:?}", other);
+                    continue;
+                }
+            };
+            packet.answers.push(rdata)
+        }
+
+        Ok(packet)
+    }
+
+    async fn resolve(&self, domain: &str, qtype: QueryType) -> Result<DnsPacket> {
+        // We only support A record for now, for other records, we just forward them to upstream.
+        if !matches!(qtype, QueryType::A | QueryType::AAAA) {
+            return self.resolve_real(domain, qtype).await;
+        }
+
+        let mut packet = DnsPacket::new();
+
+        // lookup /etc/hosts
         if let Some(ip) = self.inner.hosts.get(domain) {
             packet.answers.push(DnsRecord::A {
                 domain: domain.to_string(),
                 addr: ip,
-                ttl: TransientTtl(60),
+                ttl: TransientTtl(3),
             });
             debug!(
                 "lookup host for /etc/hosts domain: {}, ip: {:?}",
@@ -104,51 +188,22 @@ impl RuleBasedDnsResolver {
         // direct traffic bypass tun.
         let bypass_direct = self.inner.bypass_direct;
         match self.inner.rules.action_for_domain(domain) {
+            // Return real ip when `bypass_direct` is true.
             Some(Action::Direct) if bypass_direct => {
-                let lookup_ip = self.inner.resolver.lookup_ip(domain).await.map_err(|e| {
-                    let msg = e.to_string();
-                    error!("directly lookup host error: {}", &msg);
-                    io::Error::new(io::ErrorKind::Other, msg)
-                })?;
-                let mut ips: Vec<IpAddr> = vec![];
-                for record in lookup_ip.as_lookup().record_iter() {
-                    let rdata = match record.data() {
-                        Some(RData::A(ip)) => {
-                            ips.push(IpAddr::V4(*ip));
-                            DnsRecord::A {
-                                domain: domain.to_string(),
-                                addr: *ip,
-                                ttl: TransientTtl(record.ttl()),
-                            }
-                        }
-                        Some(RData::AAAA(ip)) => {
-                            ips.push(IpAddr::V6(*ip));
-                            DnsRecord::AAAA {
-                                domain: domain.to_string(),
-                                addr: *ip,
-                                ttl: TransientTtl(record.ttl()),
-                            }
-                        }
-                        other => {
-                            trace!("resolve return other: {:?}", other);
-                            continue;
-                        }
-                    };
-                    packet.answers.push(rdata)
-                }
-
-                debug!("lookup host for direct domain: {}, ip: {:?}", domain, ips);
-                return Ok(packet);
+                return self.resolve_real(domain, qtype).await;
             }
+            // Do not return dns records when action is reject.
             Some(Action::Reject) => return Ok(packet),
             _ => {}
         };
 
         let ip = if let Some(addr) = self.inner.db.get(domain).expect("get domain") {
+            // Use cached ip.
             let ip = String::from_utf8(addr.to_vec()).unwrap();
             debug!("lookup host from cache, domain: {}, ip: {}", domain, &ip);
             ip
         } else {
+            // Generate new ip.
             let ip = self.gen_ipaddr();
             debug!("lookup host gen ip, domain: {}, ip: {}", domain, &ip);
 
@@ -173,7 +228,7 @@ impl RuleBasedDnsResolver {
         packet.answers.push(DnsRecord::A {
             domain: domain.to_string(),
             addr: ip.parse().unwrap(),
-            ttl: TransientTtl(5),
+            ttl: TransientTtl(3),
         });
         Ok(packet)
     }
@@ -181,13 +236,8 @@ impl RuleBasedDnsResolver {
 
 #[async_trait]
 impl DnsResolver for RuleBasedDnsResolver {
-    async fn resolve(
-        &self,
-        domain: &str,
-        _qtype: QueryType,
-        _recursive: bool,
-    ) -> Result<DnsPacket> {
-        self.resolve(domain).await
+    async fn resolve(&self, domain: &str, qtype: QueryType, _recursive: bool) -> Result<DnsPacket> {
+        self.resolve(domain, qtype).await
     }
 
     fn as_any(&self) -> &dyn Any {
@@ -217,12 +267,16 @@ mod tests {
             )
             .await;
             assert_eq!(
-                resolver.resolve("baidu.com").await.unwrap().get_random_a(),
+                resolver
+                    .resolve("baidu.com", QueryType::A)
+                    .await
+                    .unwrap()
+                    .get_random_a(),
                 Some("10.0.0.1".to_string())
             );
             assert_eq!(
                 resolver
-                    .resolve("www.ali.com")
+                    .resolve("www.ali.com", QueryType::A)
                     .await
                     .unwrap()
                     .get_random_a(),
@@ -231,6 +285,14 @@ mod tests {
             assert_eq!(
                 resolver.lookup_host("10.0.0.1"),
                 Some("baidu.com".to_string())
+            );
+            assert_eq!(
+                resolver
+                    .resolve("mycookbook.allsunday.io", QueryType::TXT)
+                    .await
+                    .unwrap()
+                    .get_txt(),
+                Some("172.16.162.10".to_string())
             );
             assert_eq!(resolver.lookup_host("10.1.0.1"), None);
         });
