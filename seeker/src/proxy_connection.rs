a5723e29664f58cf2952675b32ef526eb5867bfc
Alex.F <gfreezy@gmail.com>
2023-02-11 12:06:01 +0800

    add connection id

--- seeker/src/proxy_connection.rs
+++ seeker/src/proxy_connection.rs
@@ -1,9 +1,18 @@
+use std::sync::atomic::AtomicU64;
 use std::time::{Duration, Instant};
 
 use crate::traffic::Traffic;
 use config::{rule::Action, Address, ServerConfig};
 
+// id generator for connection
+pub static CONNECTION_ID: AtomicU64 = AtomicU64::new(0);
+
+pub fn next_connection_id() -> u64 {
+    CONNECTION_ID.fetch_add(1, std::sync::atomic::Ordering::SeqCst)
+}
+
 pub trait ProxyConnection {
+    fn id(&self) -> u64;
     fn traffic(&self) -> Traffic;
     fn action(&self) -> Action;
     fn config(&self) -> Option<&ServerConfig>;
