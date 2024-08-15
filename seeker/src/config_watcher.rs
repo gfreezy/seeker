2789b2f904fadd347896ac70fd25bfe6d0cb2fb9
Alex.F <gfreezy@gmail.com>
2024-08-15 18:26:59 +0800

    Refactor config_watcher to use RecommendedWatcher

--- seeker/src/config_watcher.rs
+++ seeker/src/config_watcher.rs
@@ -1,12 +1,13 @@
 use std::path::PathBuf;
 use std::time::Duration;
 
-use notify_debouncer_mini::{new_debouncer, notify::*, DebounceEventHandler, Debouncer};
+use notify_debouncer_mini::notify::{RecommendedWatcher, RecursiveMode};
+use notify_debouncer_mini::{new_debouncer, DebounceEventHandler, Debouncer};
 
 pub fn watch_config<F: DebounceEventHandler + Send>(
     config_path: PathBuf,
     action: F,
-) -> Debouncer<FsEventWatcher> {
+) -> Debouncer<RecommendedWatcher> {
     let mut debouncer =
         new_debouncer(Duration::from_secs(1), action).expect("create debouncer error");
 
